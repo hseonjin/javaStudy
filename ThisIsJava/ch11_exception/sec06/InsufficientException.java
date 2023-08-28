c3078ce40740dbe18951d0aad6d1ce1d07abba3f
Seonjin Heo <seonjin.dev@gmail.com>
2023-08-29 01:18:42 +0900

    [230828] Study the Exception

---
+++ ThisIsJava/ch11_exception/sec06/InsufficientException.java
@@ -0,0 +1,10 @@
+package sec06;
+
+public class InsufficientException extends Exception {
+	public InsufficientException() {
+	}
+
+	public InsufficientException(String message) {
+		super(message);
+	}
+}
